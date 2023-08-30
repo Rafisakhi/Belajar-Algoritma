8f551e04f9a73156c2035bf36c5feaa54065e183
rafisakhi <rafisakhi57@gmail.com>
2023-08-30 15:08:59 +0700

    dasar1

---
+++ dasar1.js
@@ -0,0 +1,72 @@
+// Variable yang menampung tipe data string (text), interger / boolean array object
+
+let nama = "Rafi";
+let umur = 18;
+let apakahBenar = true;
+let uang = 5500.5;
+
+// opertator aritmatika
+let angka1 = 10;
+let angka2 = 20;
+
+console.log(angka1 + angka2);
+console.log(angka1 * angka2);
+console.log(angka1 / angka2);
+console.log(angka1 - angka2);
+console.log(angka1 % angka2);
+
+let penampunganHasiAritmatika = angka1 + angka2;
+console.log(penampunganHasiAritmatika);
+
+penampunganHasiAritmatika = angka1 * angka2;
+console.log(penampunganHasiAritmatika);
+
+penampunganHasiAritmatika = angka1 / angka2;
+console.log(penampunganHasiAritmatika);
+
+penampunganHasiAritmatika = angka1 - angka2;
+console.log(penampunganHasiAritmatika);
+
+penampunganHasiAritmatika = angka1 % angka2;
+console.log(penampunganHasiAritmatika);
+
+//  string juga bisa ditambah namun string ga bisa dikurangi dengan operator aritmatika
+
+let fristName = "Muhammad";
+let lastName = "Rafi";
+let fullName = fristName + " " + lastName;
+console.log(fullName);
+
+console.log(fristName + " " + lastName);
+
+// Alur Pembacaan code
+// Untuk membaca code bisa dimulai dari kiri ke kanan mulai dari atas ke bawah
+
+// contoh
+
+let number = 20;
+console.log(number + 10);
+number = 10;
+console.log(number + 10);
+number = false;
+
+// Pertanyaan ? kenapa false + 10 tetap menjadi 10 ?? karena false = 0 dan true = 1
+
+console.log(number + 10);
+console.log(true + 10); // 11
+
+//Membaca Error
+// error itu jangan kalian takut, justru takut kalau ada tidak ada yang namanya errro dalam pemprogramana
+// karena dalam pekerjaan kalau suatu fitur berjalan namun ada bug tapi tidak error, itu bikin si programer rada
+// muak buat mencari errornya atau letak kesalahannya
+
+// contoh error
+// const hewan = "sapi";
+// console.log(hewan);
+// hewan = "kambing";
+// console.log(hewan);
+
+// auto komentar bisa dengan mencet CTRL + /
+
+// Tipsnya adalah selalu teliti dan kalau ada error dibaca aja dan belajar ngoding itu
+// perlu kebiasaan karena ngoding itu adalah habit
---
+++ script.js
@@ -0,0 +1,14 @@
+console.log("Hello World!");
+
+/**
+ 
+Algoritma Mengganti Lampu 
+
+Langkah Pertama Kalian Matikan Saklar Lampu Tersebut Agar Aman
+
+yang Kedua Kalian Cari tangga untuk melepas Lampu Tersebut Dari
+Tempat lampu tersebut 
+
+Jika sudah mendapatkan tangga kalian bisa melepas lampu tersebut 
+dan mengganti dengan lampu yang masi menyala
+ */
